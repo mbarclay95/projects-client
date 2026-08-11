f3eb458d65aa12f7cdc04b10a92a7971115f0c30
Michael Barclay <michaelbarclay36@gmail.com>
2026-08-11 19:44:49 +0000

    somehow missed this

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -2,6 +2,6 @@ export const environment = {
   production: true,
   apiUrl: 'https://home-api.bigmike.dev/api',
   // Old hostname: signup links already sent to people contain it and never expire.
-  publicApiUrl: 'home-api-public.bigmike.dev/api',
+  publicApiUrl: 'https://home-api-public.bigmike.dev/api',
   moneyAppApiUrl: 'https://money-api.bigmike.dev/api',
 };
